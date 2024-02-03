253a8b966720a2bdee2aec9f0191d1f5884f3d29
Zaid Shahzad <91172567+ZaidShahzad@users.noreply.github.com>
2024-02-02 22:28:36 -0500

    Update Program.py

--- src/Program.py
+++ src/Program.py
@@ -28,6 +28,7 @@ from PyQt5.QtCore import Qt
 from thunkd.thunkd import pull, push, getThunkableToken
 import Utils
 from PyQt5.QtWidgets import QGridLayout
+import os
 
 class MyApp(QWidget):
     def __init__(self):
